5b5bc3f1e00ae9e7959c88ea214e9bc09b8cd40a
r <rmartin1194@gmail.com>
2024-12-13 21:25:18 +0800

    changes

--- Session.vb
+++ Session.vb
@@ -1,16 +1,12 @@
 ﻿Public Class Session
-    ' Private static instance
     Private Shared _instance As Session
 
-    ' Properties to store session data
     Public Property CurrentUserRole As String
     Public Property CurrentUserPersonalID As String
 
-    ' Private constructor to prevent direct instantiation
     Private Sub New()
     End Sub
 
-    ' Public method to access the singleton instance
     Public Shared Function GetInstance() As Session
         If _instance Is Nothing Then
             _instance = New Session()
