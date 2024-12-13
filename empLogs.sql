69eb686bdb1535dfff43f26dd7ae0126e15bdf88
v1nux <70435029+v1nux@users.noreply.github.com>
2024-12-13 12:02:26 +0800

    added empLogs

---
+++ empLogs.sql
@@ -0,0 +1,17 @@
+-- create table empLogs (
+--     empID VARCHAR(30) NOT NULL,
+--     FirstName VARCHAR(50) NOT NULL,
+--     LastName VARCHAR(50) NOT NULL,
+--     Shifts DATETIME
+-- );
+
+-- INSERT INTO empLogs (empID, FirstName, LastName, Shifts)
+-- VALUES
+-- ('E001', 'John', 'Doe', '2024-12-13 09:00:00'),
+-- ('E001', 'John', 'Doe', '2024-12-15 10:00:00'),
+-- ('E002', 'Jane', 'Smith', '2024-12-13 10:00:00'),
+-- ('E003', 'Robert', 'Johnson', '2024-12-13 11:00:00'),
+-- ('E004', 'Emily', 'Davis', '2024-12-13 12:00:00'),
+-- ('E005', 'Michael', 'Brown', '2024-12-13 13:00:00');
+
+-- select * from empLogs	
